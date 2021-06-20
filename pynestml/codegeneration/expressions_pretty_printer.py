f27e897c7c9a004c2a4a077c726f5af7c92e6139
C.A.P. Linssen <charl@turingbirds.com>
2021-06-20 09:36:04 +0200

    fix/refactor types pretty printer in code generator

--- pynestml/codegeneration/expressions_pretty_printer.py
+++ pynestml/codegeneration/expressions_pretty_printer.py
@@ -18,7 +18,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
-from pynestml.codegeneration.i_reference_converter import IReferenceConverter
+
+from typing import Tuple
+
+from pynestml.codegeneration.cpp_types_printer import CppTypesPrinter
 from pynestml.codegeneration.nestml_reference_converter import NestMLReferenceConverter
 from pynestml.meta_model.ast_expression import ASTExpression
 from pynestml.meta_model.ast_expression_node import ASTExpressionNode
@@ -28,7 +31,7 @@ from pynestml.symbols.predefined_functions import PredefinedFunctions
 from pynestml.utils.ast_utils import ASTUtils
 
 
-class ExpressionsPrettyPrinter(object):
+class ExpressionsPrettyPrinter:
     """
     Converts expressions to the executable platform dependent code. By using different
     referenceConverters for the handling of variables, names, and functions can be adapted. For this,
@@ -37,8 +40,6 @@ class ExpressionsPrettyPrinter(object):
     """
 
     def __init__(self, reference_converter=None, types_printer=None):
-        # type: (IReferenceConverter,TypesPrinter) -> None
-        # todo by kp: this should expect a ITypesPrinter as the second arg
         if reference_converter is not None:
             self.reference_converter = reference_converter
         else:
@@ -47,7 +48,7 @@ class ExpressionsPrettyPrinter(object):
         if types_printer is not None:
             self.types_printer = types_printer
         else:
-            self.types_printer = TypesPrinter()
+            self.types_printer = CppTypesPrinter()
 
     def print_expression(self, node, prefix=''):
         """Print an expression.
@@ -70,8 +71,7 @@ class ExpressionsPrettyPrinter(object):
         else:
             return self.__do_print(node, prefix=prefix)
 
-    def __do_print(self, node, prefix=''):
-        # type: (ASTExpressionNode) -> str
+    def __do_print(self, node: ASTExpressionNode, prefix: str='') -> str:
         if isinstance(node, ASTSimpleExpression):
             if node.has_unit():
                 # todo by kp: this should not be done in the typesPrinter, obsolete
@@ -91,6 +91,7 @@ class ExpressionsPrettyPrinter(object):
                 return self.reference_converter.convert_name_reference(node.get_variable(), prefix=prefix)
             elif node.is_function_call():
                 return self.print_function_call(node.get_function_call(), prefix=prefix)
+            raise Exception('Unknown node type')
         elif isinstance(node, ASTExpression):
             # a unary operator
             if node.is_unary_operator():
@@ -99,7 +100,8 @@ class ExpressionsPrettyPrinter(object):
                 return op % rhs
             # encapsulated in brackets
             elif node.is_encapsulated:
-                return self.reference_converter.convert_encapsulated() % self.print_expression(node.get_expression(), prefix=prefix)
+                return self.reference_converter.convert_encapsulated() % self.print_expression(node.get_expression(),
+                                                                                               prefix=prefix)
             # logical not
             elif node.is_logical_not:
                 op = self.reference_converter.convert_logical_not()
@@ -116,6 +118,7 @@ class ExpressionsPrettyPrinter(object):
                 if_true = self.print_expression(node.get_if_true(), prefix=prefix)
                 if_not = self.print_expression(node.if_not, prefix=prefix)
                 return self.reference_converter.convert_ternary_operator() % (condition, if_true, if_not)
+            raise Exception('Unknown node type')
         else:
             raise RuntimeError('Unsupported rhs in rhs pretty printer (%s)!' % str(node))
 
@@ -138,32 +141,17 @@ class ExpressionsPrettyPrinter(object):
         """
         function_name = self.reference_converter.convert_function_call(function_call, prefix=prefix)
         if ASTUtils.needs_arguments(function_call):
-            return function_name.format(*self.print_function_call_argument_list(function_call, prefix=prefix))
+            if function_call.get_name() == PredefinedFunctions.PRINT or function_call.get_name() == PredefinedFunctions.PRINTLN:
+                return function_name.format(self.reference_converter.convert_print_statement(function_call))
+            else:
+                return function_name.format(*self.print_function_call_argument_list(function_call, prefix=prefix))
         else:
             return function_name
 
-    def print_function_call_argument_list(self, function_call, prefix=''):
-        # type: (ASTFunctionCall) -> tuple of str
+    def print_function_call_argument_list(self, function_call: ASTFunctionCall, prefix: str='') -> Tuple[str, ...]:
         ret = []
 
         for arg in function_call.get_args():
             ret.append(self.print_expression(arg, prefix=prefix))
 
         return tuple(ret)
-
-
-class TypesPrinter(object):
-    """
-    Returns a processable format of the handed over element.
-    """
-
-    @classmethod
-    def pretty_print(cls, element):
-        assert (element is not None), \
-            '(PyNestML.CodeGeneration.PrettyPrinter) No element provided (%s)!' % element
-        if isinstance(element, bool) and element:
-            return 'true'
-        elif isinstance(element, bool) and not element:
-            return 'false'
-        elif isinstance(element, int) or isinstance(element, float):
-            return str(element)
