cd4f8cd9739a1ab0c44caa7600acd5b3834e8b50
C.A.P. Linssen <charl@turingbirds.com>
2022-01-15 15:44:31 +0100

    clean up NEST/GSL reference converter

--- pynestml/codegeneration/nest_code_generator.py
+++ pynestml/codegeneration/nest_code_generator.py
@@ -130,7 +130,7 @@ class NESTCodeGenerator(CodeGenerator):
         self.gsl_reference_converter = GSLReferenceConverter()
         self.gsl_printer = UnitlessExpressionPrinter(self.gsl_reference_converter)
 
-        self.nest_reference_converter = NESTReferenceConverter(False)
+        self.nest_reference_converter = NESTReferenceConverter()
         self.unitless_printer = UnitlessExpressionPrinter(self.nest_reference_converter)
 
     def raise_helper(self, msg):
