43ae950ce108cd2b43f19fa72e2cd785d42e076f
C.A.P. Linssen <charl@turingbirds.com>
2022-01-16 16:24:36 +0100

    fix CI after merge of to_nest and install_nest into generate_target

--- tests/nest_tests/resources/print_variable_script.py
+++ tests/nest_tests/resources/print_variable_script.py
@@ -23,11 +23,12 @@ import os
 import nest
 import shutil
 
-from pynestml.frontend.pynestml_frontend import to_nest, install_nest
+from pynestml.frontend.pynestml_frontend import generate_target
 
 input_path = str(os.path.realpath(os.path.join(os.path.dirname(__file__), "PrintVariables.nestml")))
 nest_path = nest.ll_api.sli_func("statusdict/prefix ::")
 target_path = "../target"
+target_platform = "NEST"
 logging_level = "INFO"
 module_name = "nestmlmodule"
 store_log = False
@@ -35,7 +36,7 @@ suffix = "_nestml"
 dev = True
 codegen_opts = {"nest_path": nest_path}
 
-to_nest(input_path, target_path, logging_level, module_name, store_log, suffix, dev, codegen_opts)
+generate_target(input_path, target_path, target_platform, logging_level, module_name, store_log, suffix, dev, codegen_opts)
 nest.set_verbosity("M_ALL")
 
 nest.ResetKernel()
