371d76fdc7fe8448c19771660631f06dddab44a8
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 17:19:48 +0000

    add Benchmark (pytest) benchmark result for 5d4bcb29762d13246e4c576146f367bebbbdc1a9

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770225588583,
+  "repoUrl": "https://github.com/clinssen/nestml",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "clinssen",
+            "username": "clinssen"
+          },
+          "committer": {
+            "name": "clinssen",
+            "username": "clinssen"
+          },
+          "id": "5d4bcb29762d13246e4c576146f367bebbbdc1a9",
+          "message": "update gitignore",
+          "timestamp": "2021-12-15T16:30:19Z",
+          "url": "https://github.com/clinssen/nestml/pull/19/commits/5d4bcb29762d13246e4c576146f367bebbbdc1a9"
+        },
+        "date": 1770225587753,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/nest_continuous_benchmarking/test_nest_continuous_benchmarking.py::TestNESTContinuousBenchmarking::test_stdp_nn_synapse",
+            "value": 0.3243865130203272,
+            "unit": "iter/sec",
+            "range": "stddev: 0.18536383516880647",
+            "extra": "mean: 3.0827422222000225 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
